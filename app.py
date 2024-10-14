503559cba601679edac7009ea9a9fd555f8b15bd
Angely <angelygrisales@gmail.com>
2024-10-15 10:17:46 +1100

    agregue la logica del histograma a la web

---
+++ app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import plotly_express as px
+import streamlit as st
+
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button=st.button ("Construir Histograma")
+scatter_plot_button=st.button("Construir Grafico de Dispersión")
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    fig = px.histogram(car_data, x="odometer") # crear un histograma
+    st.plotly_chart(fig, use_container_width=True)
+    fig.show() # crear gráfico de dispersión
+if scatter_plot_button:
+   st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+   fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+   fig.show() # crear gráfico de dispersión
+ 
+        
