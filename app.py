63b0a67c1166d5fe9cb49c0a62b665db4cab4e67
Angely <angelygrisales@gmail.com>
2024-10-15 12:29:56 +1100

    ajustes finales

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ import pandas as pd
 import plotly.express as px
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+st.header("Histograma ventas de coches")
 hist_button=st.button ("Construir Histograma")
-scatter_plot_button=st.button("Construir Grafico de Dispersión")
+
 if hist_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
-    fig = px.histogram(car_data, x="odometer") # crear un histograma
+    fig = px.histogram(car_data, x="odometer",nbins=20) # crear un histograma
     st.plotly_chart(fig, use_container_width=True)
     fig.show() # crear gráfico de dispersión
+
+scatter_plot_button=st.button("Construir Grafico de Dispersión")
 if scatter_plot_button:
    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
